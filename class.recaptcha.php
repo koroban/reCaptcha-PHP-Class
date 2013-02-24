dd21058153f99187c31c74e7288660178fc99257
Koroban <dev@novutec.com>
2013-02-24 21:26:37 +0100

    fixed PHPDoc syntax of @param.

--- class.recaptcha.php
+++ class.recaptcha.php
@@ -21,7 +21,7 @@ abstract class reCaptchaBase {
 
     /**
      * curl resource instance
-     * 
+     *
      * @var resource
      */
     private $ch;
@@ -37,10 +37,10 @@ abstract class reCaptchaBase {
      * Constructor
      * define private and public key for requests.
      *
-     * @param $public_key string key to use as public key
-     * @param $private_key string key to use as private key
-     * @param $init_curl bool init curl instance
-     * 
+     * @param string $public_key  key to use as public key
+     * @param string $private_key key to use as private key
+     * @param bool   $init_curl   init curl instance
+     *
      * @throws InvalidArgumentException
      *
      * @return reCaptcha
@@ -89,8 +89,8 @@ abstract class reCaptchaBase {
     /**
      * send post call to remote api server
      *
-     * @param $url string url to send call to
-     * @param $data array post parameter for call
+     * @param string $url  url to send call to
+     * @param array  $data post parameter for call
      *
      * @throws RuntimeException
      *
@@ -130,9 +130,9 @@ class reCaptcha extends reCaptchaBase {
      * This is called from the browser, and the resulting reCAPTCHA HTML widget
      * is embedded within the HTML form it was called from.
      *
-     * @param string $error The error given by reCAPTCHA (optional, default is null)
+     * @param string  $error   The error given by reCAPTCHA (optional, default is null)
      * @param boolean $use_ssl Should the request be made over ssl? (optional, default is false)
-     * @param array $options List of options to set to reCatpcha JavaScript engine
+     * @param array   $options List of options to set to reCatpcha JavaScript engine
 
      * @return string - The HTML to be embedded in the user's form.
      */
@@ -142,15 +142,15 @@ class reCaptcha extends reCaptchaBase {
         $url = 'http' . ($use_ssl ? 's' : '') . '://' . self::$api_server;
 
         if (!empty($options)) {
-            $out .= '<script type="text/javascript">' . 
+            $out .= '<script type="text/javascript">' .
                     'var RecaptchaOptions = ' . json_encode($options) . ';</script>';
         }
 
-        $out .= '<script type="text/javascript" src="' . $url . '/challenge?k=' . $this->public_key . 
+        $out .= '<script type="text/javascript" src="' . $url . '/challenge?k=' . $this->public_key .
                ($error ? '&error=' . urlencode($error) : '') . '"></script>';
 
-        $out .= '<noscript>' . 
-                '<iframe src="' . $url . '/noscript?k=' . $this->public_key . 
+        $out .= '<noscript>' .
+                '<iframe src="' . $url . '/noscript?k=' . $this->public_key .
                 ($error ? '&error=' . urlencode($error) : '') .
                 '" height="300" width="500" frameborder="0"></iframe><br/>' .
                 '<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>' .
@@ -163,10 +163,10 @@ class reCaptcha extends reCaptchaBase {
     /**
       * Calls an HTTP POST function to verify if the user's guess was correct
       *
-      * @param string $remote_ip client remote ip, needed for validation
-      * @param string $challenge api chalange
-      * @param string $response api response
-      * @param array $extra_params an array of extra variables to post to the server
+      * @param string $remote_ip    client remote ip, needed for validation
+      * @param string $challenge    api chalange
+      * @param string $response     api response
+      * @param array  $extra_params an array of extra variables to post to the server
       *
       * @throws InvalidArgumentException
       * @throws UnexpectedValueException
@@ -186,7 +186,7 @@ class reCaptcha extends reCaptchaBase {
             return false;
         }
 
-        $res = $this->__call('http://' . self::$api_server . '/verify', 
+        $res = $this->__call('http://' . self::$api_server . '/verify',
             array_merge(array(
                 'privatekey' => $this->private_key,
                 'remoteip' => $remote_ip,
@@ -230,13 +230,14 @@ class MailHide extends reCaptchaBase
 {
     /**
      * Custom constructor to disable init of curl
-     * @param $public_key string key to use as public key
-     * @param $private_key string key to use as private key
-     * 
+     *
+     * @param string $public_key  key to use as public key
+     * @param string $private_key key to use as private key
+     *
      * @throw InvalidArgumentException if private/public key missing
      *
      * @return reCaptcha
-     */ 
+     */
     public function __construct($public_key, $private_key) {
         return parent::__construct($public_key, $private_key, false);
     }
@@ -275,7 +276,7 @@ class MailHide extends reCaptchaBase
     /**
      * convert encrypted mail to base64 encoded
      *
-     * @param $x string input to convert
+     * @param string $x input to convert
      *
      * @return string converted base64
      */
@@ -285,9 +286,9 @@ class MailHide extends reCaptchaBase
     }
 
     /**
-     * gets the reCAPTCHA Mailhide url for a given email, public key and private key 
+     * gets the reCAPTCHA Mailhide url for a given email, public key and private key
      *
-     * @param $email string email to use
+     * @param string $email email to use
      *
      * @return string url of google recaptcha api
      */
@@ -295,7 +296,7 @@ class MailHide extends reCaptchaBase
     {
         $ky         = pack('H*', $this->private_key);
         $crypt_mail = $this->_aes_encrypt($email, $ky);
-        return 'http://www.google.com/recaptcha/mailhide/d?k=' . $this->public_key . 
+        return 'http://www.google.com/recaptcha/mailhide/d?k=' . $this->public_key .
                '&c=' . $this->_get_mailhide_urlbase64($crypt_mail);
     }
 
@@ -304,7 +305,7 @@ class MailHide extends reCaptchaBase
      * eg, given johndoe@example,com return ["john", "example.com"].
      * the email is then displayed as john...@example.com
      *
-     * @param $email string email to explode
+     * @param string $email email to explode
      *
      * @return array of elements.
      */
@@ -327,7 +328,7 @@ class MailHide extends reCaptchaBase
      *
      * @see http://www.google.com/recaptcha/mailhide/apikey
      *
-     * @param $email string email to convert
+     * @param string $email email to convert
      *
      * @return string converted version
      */
@@ -335,10 +336,10 @@ class MailHide extends reCaptchaBase
     {
         $email_parts = $this->_get_mailhide_email_parts($email);
         $url         = $this->_get_mailhide_url($email);
-        return htmlentities($email_parts[0]) . 
-               '<a href="' . htmlentities($url) . '" onclick="window.open(\'' . htmlentities($url) . 
+        return htmlentities($email_parts[0]) .
+               '<a href="' . htmlentities($url) . '" onclick="window.open(\'' . htmlentities($url) .
                '\', \'\', \'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,'.
-               'width=500,height=300\'); return false;" title="Reveal this e-mail address">...</a>@' . 
+               'width=500,height=300\'); return false;" title="Reveal this e-mail address">...</a>@' .
                htmlentities($email_parts[1]);
     }
 }
