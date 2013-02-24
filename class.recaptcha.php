a88a77e3955641364468a17e7b4fa45c2b838fc3
Koroban <dev@novutec.com>
2013-02-24 15:00:11 +0100

    rewritten recaptcha class to use curl extension and correct OOP.

--- class.recaptcha.php
+++ class.recaptcha.php
@@ -1,199 +1,287 @@
 <?php
+abstract class reCaptchaBase {
+    private $public_key;
+    private $private_key;
+    private $ch;
+    private $api_server = 'www.google.com/recaptcha/api';
 
-class reCaptcha {
-	
-	private $public_key = WEBSITE_RECAPTCHA_PUBLIC_KEY;
-	private $private_key = WEBSITE_RECAPTCHA_PRIVATE_KEY;
-	
-	var $api_create = 'https://www.google.com/recaptcha/admin/create';
-	var $api_server = 'http://www.google.com/recaptcha/api';
-	var $api_secure_server = 'https://www.google.com/recaptcha/api';
-	var $verify_server = 'www.google.com';
-	var $is_valid;
-	var $error_message;
-	
-	private function encode_query($data){
-		
-		$request = '';
-		
-		foreach($data as $key => $value) $request .= $key.'='.urlencode(stripslashes($value)).'&';
-		
-		// cut the last '&'
-		$request = substr($request, 0, strlen($request) - 1);
-		
-		return $request;
-		
-	}
-	
-	private function post($host, $path, $data, $port = 80){
+    /**
+     * Constructor
+     * define private and public key for requests.
+     *
+     * @param $public_key string key to use as public key
+     * @param $private_key string key to use as private key
+     * @param $init_curl bool init curl instance
+     * 
+     * @throw InvalidArgumentException if private/public key missing
+     *
+     * @return reCaptcha
+     */
+    public function __construct($public_key, $private_key, $init_curl = true)
+    {
+        $this->public_key = $public_key;
+        $this->private_key = $private_key;
+        if (empty($this->public_key) || empty($this->private_key)) {
+            throw new InvalidArgumentException('Invalid private / public key.');
+        }
 
-		$request = $this->encode_query($data);
-		
-		$http_request  = "POST $path HTTP/1.0\r\n";
-		$http_request .= "Host: $host\r\n";
-		$http_request .= "Content-Type: application/x-www-form-urlencoded;\r\n";
-		$http_request .= "Content-Length: ".strlen($request)."\r\n";
-		$http_request .= "User-Agent: reCAPTCHA/PHP\r\n";
-		$http_request .= "\r\n";
-		$http_request .= $request;
-		
-		$response = '';
-		
-		if(false == ($fs = fsockopen($host, $port, $errno, $errstr, 10))) exit('reCaptcha: Could not open socket');
-		
-		fwrite($fs, $http_request);
-		
-		while(!feof($fs)) $response .= fgets($fs, 1160); // One TCP-IP packet
-		
-		fclose($fs);
-		
-		$response = explode("\r\n\r\n", $response, 2);
-		
-		return $response;
-		
-	}
-	
-	public function form($error = false, $use_ssl = false){
-		
-		if(is_null($this->public_key) || empty($this->public_key)) exit('To use reCAPTCHA you must get an API key from <a href="'.$this->api_create.'">'.$this->api_create.'</a>');
-		
-		$server = ($use_ssl ? $this->$api_secure_server : $this->api_server);
-	
-		$errorpart = '';
-		
-		if($error) $errorpart = '&amp;error='.$error;
-		
-		echo '
-		<script type="text/javascript" src="'.$server.'/challenge?k='.$this->public_key.$errorpart.'"></script>
-		<noscript>
-			<iframe src="'.$server. '/noscript?k='.$this->public_key.$errorpart.'" height="300" width="500" frameborder="0"></iframe><br/>
-			<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
-			<input type="hidden" name="recaptcha_response_field" value="manual_challenge"/>
-		</noscript>';
-		
-	}
-	
-	public function check_answer($remote_ip, $challenge, $response, $extra_params = array()){
-		
-		if(is_null($this->private_key) || empty($this->private_key)) exit('To use reCAPTCHA you must get an API key from <a href="'.$this->api_create.'">'.$this->api_create.'</a>');
-		
-		if(is_null($remote_ip) || empty($remote_ip)) exit('For security reasons, you must pass the remote IP to reCAPTCHA');
-		
-		// discard spam submissions
-		if(is_null($challenge) || strlen($challenge) == 0 || is_null($response) || strlen($response) == 0){		
-			$this->is_valid = false;
-			$this->error_message = 'incorrect-captcha-sol';
-			return;
-		}
-		
-		$response = $this->post(
-			$this->verify_server, 
-			'/recaptcha/api/verify',
-			array(
-				'privatekey' => $this->private_key,
-				'remoteip' => $remote_ip,
-				'challenge' => $challenge,
-				'response' => $response
-			) + $extra_params
-		);
-		
-		$answers = explode ("\n", $response[1]);
-		
-		if(trim($answers[0]) == 'true'){
-		
-			$this->is_valid = true;
-		
-		}else{
-		
-			$this->is_valid = false;
-			$this->error_message = $answers [1];
-		
-		}
-		
-		return ($this->is_valid ? $this->is_valid : false);
-		
-	}	
-	
-	public function get_signup_url($domain = false, $appname = false){
-		
-		return $this->api_create.'?'.$this->encode_query(array('domains' => $domain, 'app' => $appname));
-		
-	}
-	
+        if ($init_curl) {
+            $this->ch = self::_initCurl();
+        }
+    }
+
+    /**
+     * Destructor
+     * clean up opened curl instance
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        curl_close($this->ch);
+    }
+
+    /**
+     * init global curl object
+     *
+     * @return resource curl instance
+     */
+    private static function _initCurl()
+    {
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_HEADER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => true,
+            CURLOPT_POST => true
+        ));
+        return $ch;
+    }
+
+    /**
+     * send post call to remote api server
+     *
+     * @param $url string url to send call to
+     * @param $data array post parameter for call
+     *
+     * @throw RuntimeException if invalid curl response
+     *
+     * @return string result of post call
+     */
+    private function _call($url, $data = array())
+    {
+        curl_setopt_array($this->ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_POSTFIELDS => $data
+        ));
+
+        $result = curl_exec($this->ch);
+        if ($result === false) {
+            throw new RuntimeException(curl_error($this->ch), curl_errno($this->ch));
+        }
+
+        return $result;
+    }
 }
 
-class MailHide {
-	
-	private $public_key = WEBSITE_MAILHIDE_PUBLIC_KEY;
-	private $private_key = WEBSITE_MAILHIDE_PRIVATE_KEY;
+class reCaptcha extends reCaptchaBase {
+    private $error_message;
 
-	private function aes_pad($val){
-		
-		$block_size = 16;
-		
-		$numpad = $block_size - (strlen($val) % $block_size);
-		
-		return str_pad($val, strlen($val) + $numpad, chr($numpad));
-		
-	}
+    /**
+     * Gets the challenge HTML (javascript and non-javascript version).
+     * This is called from the browser, and the resulting reCAPTCHA HTML widget
+     * is embedded within the HTML form it was called from.
+     *
+     * @param string $error The error given by reCAPTCHA (optional, default is null)
+     * @param boolean $use_ssl Should the request be made over ssl? (optional, default is false)
+     * @param array $options List of options to set to reCatpcha JavaScript engine
 
-	private function aes_encrypt($val, $ky){
-		
-		if(!function_exists('mcrypt_encrypt')) exit('To use reCAPTCHA Mailhide, you need to have the mcrypt php module installed.');
-		
-		$mode = MCRYPT_MODE_CBC;  		
-		$enc = MCRYPT_RIJNDAEL_128;
-		$val = $this->aes_pad($val);
-		
-		return mcrypt_encrypt($enc, $ky, $val, $mode, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
-		
-	}
+     * @return string - The HTML to be embedded in the user's form.
+     */
+    public function form($error = false, $use_ssl = false, $options = array())
+    {
+        $out = '';
+        $url = 'http' . ($use_ssl ? 's' : '') . '://' . $this->api_server;
 
+        if (!empty($options)) {
+            $out .= '<script type="text/javascript">' . 
+                    'var RecaptchaOptions = ' . json_encode($options) . ';</script>';
+        }
 
-	private function get_mailhide_urlbase64($x){
-		
-		return strtr(base64_encode($x), '+/', '-_');
-		
-	}
-	
-	private function get_mailhide_url($email){
-		
-		if(is_null($this->public_key) || empty($this->public_key) || is_null($this->private_key) || empty($this->private_key)) 
-			exit('To use reCAPTCHA Mailhide, you have to sign up for a public and private key, you can do so at <a href="http://www.google.com/recaptcha/mailhide/apikey">http://www.google.com/recaptcha/mailhide/apikey</a>');
+        $out .= '<script type="text/javascript" src="' . $url . '/challenge?k=' . $this->public_key . 
+               ($error ? '&error=' . url_encode($error) : '') . '"></script>';
 
-		$ky = pack('H*', $this->private_key);
-		$crypt_mail = $this->aes_encrypt($email, $ky);
-		
-		return 'http://www.google.com/recaptcha/mailhide/d?k='.$pubkey.'&c='.$this->get_mailhide_urlbase64($crypt_mail);
-		
-	}
-	
-	private function get_mailhide_email_parts($email){
-		
-		$arr = preg_split("/@/", $email);
-	
-		if(strlen($arr[0]) <= 4){
-			$arr[0] = substr($arr[0], 0, 1);
-		}elseif(strlen($arr[0]) <= 6){
-			$arr[0] = substr($arr[0], 0, 3);
-		}else{
-			$arr[0] = substr($arr[0], 0, 4);
-		}
-		
-		return $arr;
-		
-	}
-	
-	public function email($email){
-		
-		$email_parts = $this->get_mailhide_email_parts($email);
-		
-		$url = $this->get_mailhide_url($email);
-		
-		echo htmlentities($email_parts[0]).'<a href="'.htmlentities($url).'" onclick="window.open(\''.htmlentities($url).'\', \'\', \'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300\'); return false;" title="Reveal this e-mail address">...</a>@'.htmlentities($email_parts[1]);
-	
-	}	
-	
+        $out .= '<noscript>' . 
+                '<iframe src="' . $url . '/noscript?k=' . $this->public_key . 
+                ($error ? '&error=' . url_encode($error) : '') .
+                '" height="300" width="500" frameborder="0"></iframe><br/>' .
+                '<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>' .
+                '<input type="hidden" name="recaptcha_response_field" value="manual_challenge">' .
+                '</noscript>';
+
+        return $out;
+    }
+
+    /**
+      * Calls an HTTP POST function to verify if the user's guess was correct
+      *
+      * @param string $remote_ip
+      * @param string $challenge
+      * @param string $response
+      * @param array $extra_params an array of extra variables to post to the server
+      *
+      * @return bool True if valid answer
+      */
+    public function check_answer($remote_ip, $challenge, $response, $extra_params = array())
+    {
+        if (is_null($remote_ip) || empty($remote_ip)) {
+            throw new InvalidArgumentException('For security reasons, you must pass the remote IP to reCAPTCHA');
+        }
+
+        // discard spam submissions
+        if (empty($challenge) || empty($response)) {
+            $this->error_message = 'incorrect-captcha-sol';
+            return false;
+        }
+
+        $res = $this->__call('http://' . $this->api_server . '/verify', 
+            array_merge(array(
+                'privatekey' => $this->private_key,
+                'remoteip' => $remote_ip,
+                'challenge' => $challenge,
+                'response' => $response
+            ), $extra_params)
+        );
+
+        if (empty($res)) {
+            throw new UnexpectedValueException('Invalid API Response.');
+        }
+
+        $answers = explode("\n", $res);
+        if (empty($answer)) {
+            throw new UnexpectedValueException('Invalid API Response.');
+        }
+
+        if (trim($answers[0]) == 'true') {
+            return true;
+        }
+        $this->error_message = $answers[1];
+        return false;
+    }
 }
 
-?>
+class MailHide extends reCaptchaBase
+{
+    /**
+     * Custom constructor to disable init of curl
+     * @param $public_key string key to use as public key
+     * @param $private_key string key to use as private key
+     * 
+     * @throw InvalidArgumentException if private/public key missing
+     *
+     * @return reCaptcha
+     */ 
+    public function __construct($public_key, $private_key) {
+        return parent::__construct($public_key, $private_key, false);
+    }
+
+    /**
+     * generated AES padding
+     *
+     * @param string $val string to fill
+     *
+     * @return string filled string
+     */
+    private function _aes_pad($val)
+    {
+        $block_size = 16;
+        $numpad     = $block_size - (strlen($val) % $block_size);
+        return str_pad($val, strlen($val) + $numpad, chr($numpad));
+    }
+
+    /**
+     * AES Encrypt value by given key
+     *
+     * @param string $val value to encrypt
+     * @param string $ky  key to use
+     *
+     * @return string encrypted result
+     */
+    private function _aes_encrypt($val, $ky)
+    {
+        $mode = MCRYPT_MODE_CBC;
+        $enc  = MCRYPT_RIJNDAEL_128;
+        $val  = $this->aes_pad($val);
+        return mcrypt_encrypt($enc, $ky, $val, $mode, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
+    }
+
+
+    /**
+     * convert encrypted mail to base64 encoded
+     *
+     * @param $x string input to convert
+     *
+     * @return string converted base64
+     */
+    private function _get_mailhide_urlbase64($x)
+    {
+        return strtr(base64_encode($x), '+/', '-_');
+    }
+
+    /**
+     * gets the reCAPTCHA Mailhide url for a given email, public key and private key 
+     *
+     * @param $email string email to use
+     *
+     * @return string url of google recaptcha api
+     */
+    private function _get_mailhide_url($email)
+    {
+        $ky         = pack('H*', $this->private_key);
+        $crypt_mail = $this->_aes_encrypt($email, $ky);
+        return 'http://www.google.com/recaptcha/mailhide/d?k=' . $this->public_key . '&c=' . $this->_get_mailhide_urlbase64($crypt_mail);
+    }
+
+    /**
+     * gets the parts of the email to expose to the user.
+     * eg, given johndoe@example,com return ["john", "example.com"].
+     * the email is then displayed as john...@example.com
+     *
+     * @param $email string email to explode
+     *
+     * @return array of elements.
+     */
+    private function _get_mailhide_email_parts($email)
+    {
+        $arr = preg_split("/@/", $email);
+        if (strlen($arr[0]) <= 4) {
+            $arr[0] = substr($arr[0], 0, 1);
+        } elseif (strlen($arr[0]) <= 6) {
+            $arr[0] = substr($arr[0], 0, 3);
+        } else {
+            $arr[0] = substr($arr[0], 0, 4);
+        }
+        return $arr;
+    }
+
+    /**
+     * Gets html to display an email address given a public an private key.
+     * to get a key, go to:
+     *
+     * http://www.google.com/recaptcha/mailhide/apikey
+     *
+     * @param $email string email to convert
+     *
+     * @return string converted version
+     */
+    public function email($email)
+    {
+        $email_parts = $this->_get_mailhide_email_parts($email);
+        $url         = $this->_get_mailhide_url($email);
+        return htmlentities($email_parts[0]) . 
+               '<a href="' . htmlentities($url) . '" onclick="window.open(\'' . htmlentities($url) . 
+               '\', \'\', \'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,'.
+               'width=500,height=300\'); return false;" title="Reveal this e-mail address">...</a>@' . 
+               htmlentities($email_parts[1]);
+    }
+}
